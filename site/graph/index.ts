fe38273c37c12b4fde2213acbc846dc996ea4206
多牧 <xuzhi.mxz@antfin.com>
2020-11-30 16:12:32 +0800

    feat: v0.3.3 er graph

--- site/graph/index.ts
+++ site/graph/index.ts
@@ -4,12 +4,14 @@ import Mindmap from "./class/Mindmap";
 import DecisionGraph from "./class/DecisionGraph";
 import React from "./framework/React";
 import Vue from "./framework/Vue";
+import ERGraph from "./class/ERGraph";
 
 export default {
   Graph: {
     mindmap: Mindmap,
     familytree: FamilyTree,
-    decisiongraph: DecisionGraph
+    decisiongraph: DecisionGraph,
+    er: ERGraph
   },
   FrameWork: {
     react: React,
---
+++ types/graph/ERGraph.d.ts
@@ -0,0 +1,26 @@
+import { GraphOptions } from "@antv/g6/es/types";
+import DiceGraph from "../base/DiceGraph";
+declare type ERRelation = {
+    nodeId: string;
+    key: string;
+    label?: string;
+};
+declare type ERAttr = {
+    key: string;
+    type: string;
+    relation: ERRelation[];
+};
+declare type ERNode = {
+    label: string;
+    id: string;
+    attrs: ERAttr[];
+};
+export default class ERGraph extends DiceGraph<ERNode[]> {
+    constructor(userConfig: GraphOptions);
+    dataTransform(data: ERNode[]): {
+        nodes: any[];
+        edges: any[];
+    };
+    registerCustomSetting(): void;
+}
+export {};
